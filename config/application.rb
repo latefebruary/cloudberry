361976d434bb324bf51d9ed96afeb72d0a8bf1b4
Ekaterina Barasheva <ekaterina.barasheva@mkechinov.ru>
2014-11-05 21:10:28 +0300

    postmark config

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,10 @@ module Project
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    
+    # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.delivery_method   = :postmark
+    config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
