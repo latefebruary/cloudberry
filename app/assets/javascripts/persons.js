809c160f1c1c7700ac106cdbe5ae80e992a92a87
Ekaterina Barasheva <ekaterina.barasheva@mkechinov.ru>
2014-11-12 17:52:55 +0300

    registration form

---
+++ app/assets/javascripts/persons.js
@@ -0,0 +1,20 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '270986573025814',
+    xfbml      : true,
+    version    : 'v2.2'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
