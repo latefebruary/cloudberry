801c01ee9691a9d4b91f7c18e7fa931368456223
Ekaterina Barasheva <ekaterina.barasheva@mkechinov.ru>
2014-11-20 20:50:42 +0300

    omniauth twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,14 @@ class User < ActiveRecord::Base
 
   def self.new_with_session(params, session)
     super.tap do |user|
+      if data = session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
